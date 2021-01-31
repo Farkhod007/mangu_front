e40868c6dfd1f535433c258ce7d0027d741402ed
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-01-31 20:58:14 +0500

    Finish auction page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,20 @@
 let bigImage = document.querySelector("#big-image");
 let thumbnails = document.querySelectorAll(".thumbnail");
+let menu = document.querySelector(".menu");
 
 [...thumbnails].map(item => {
     item.addEventListener("click", (event) => {
+        [...thumbnails].map(el => {
+            el.classList.remove('active');
+        });
+        item.classList.toggle('active');
         bigImage.src = event.currentTarget.src;
     });
 })
+
+document.querySelector(".brand svg").addEventListener("click", () => {
+    menu.style.display = "block";
+});
+document.querySelector(".menu svg").addEventListener("click", () => {
+    menu.style.display = "none";
+});
